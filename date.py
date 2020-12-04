0300bed8620ff0fe62c2f94af816b005a12b3025
VishKapare10 <vish10kapare@gmail.com>
2020-12-04 09:26:24 -0500

    Modified file.

--- date.py
+++ date.py
@@ -2,4 +2,4 @@ import subprocess
 p = subprocess.Popen("date", stdout=subprocess.PIPE, shell=True)
 (output, err) = p.communicate()
 print("Today is", output)
-print("Today is friday!")
+print("Today is friday!!")
