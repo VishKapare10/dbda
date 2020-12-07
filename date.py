c41df1ee73a9696cccd84b243531139d46fcfbaa
VishKapare10 <56476658+VishKapare10@users.noreply.github.com>
2020-12-07 10:52:01 +0530

    Update date.py

--- date.py
+++ date.py
@@ -2,4 +2,4 @@ import subprocess
 p = subprocess.Popen("date", stdout=subprocess.PIPE, shell=True)
 (output, err) = p.communicate()
 print("Today is", output)
-print("Today is Monday(2020)!!")
+print("Today is Monday....(2020)!!")
