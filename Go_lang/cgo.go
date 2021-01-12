bd2ac890f1a837a1e0c692a7aa6fdf9111b1c610
VishKapare10 <56476658+VishKapare10@users.noreply.github.com>
2021-01-12 11:49:00 +0530

    Update cgo.go

--- Go_lang/cgo.go
+++ Go_lang/cgo.go
@@ -1,14 +1,16 @@
 package main
-
 /*
-extern void display();
-extern int add(int, int);
+extern void display_msg();
+extern int add_integers(int, int);
+extern int start(int, int);
 */
 import "C"
 import "fmt"
 
+
 func main() {
-    C.display()
-    result := C.add(20,50)
+    C.display_msg()
+    result := C.add_integers(100,50)
     fmt.Println("Addition =", result)
+    C.start(100,50);
 }
