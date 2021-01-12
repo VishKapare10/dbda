a951471d332ddaa3bdd8f990bfe531f9836ffd24
VishKapare10 <56476658+VishKapare10@users.noreply.github.com>
2021-01-12 11:48:31 +0530

    Update cgo.c

--- Go_lang/cgo.c
+++ Go_lang/cgo.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
-void display() {
-    printf("In display method!\n");
+
+void display_msg() {
+    printf("In display msg method!\n");
 }
 
-int add(int a, int b) {
+void print(int a)
+{
+    printf("Value of a is %d\n", a);
+}
+
+
+int add_integers(int a, int b) {
+  void (*fun_ptr)(int) = &print;
+  (*fun_ptr)(1500);
   return a+b;
 }
